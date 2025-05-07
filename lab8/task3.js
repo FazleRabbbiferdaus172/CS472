cebae7177b6d174b7025302734986acd3cdb10ce
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-05-06 22:07:06 -0500

    task3 updated

--- lab8/task3.js
+++ lab8/task3.js
@@ -1,11 +1,19 @@
 "use strict"
 
 async function getRecipes(url, limit=1) {
-    let response = await fetch(url)
     const decoder = new TextDecoder("utf-8")
-    for await (const chunk of response.body) 
-    {
-        console.log(decoder.decode(chunk, {stream: true}));
+    let response
+    try {
+        response = await fetch(url)
+        if (!response.ok) {
+            console.log(`HTTP error: ${response.status}`);
+        }
+        for await (const chunk of response.body) 
+        {
+            console.log(decoder.decode(chunk, {stream: true}));
+        }
+    } catch (err) {
+        console.error("Error")
     }
 }
 
